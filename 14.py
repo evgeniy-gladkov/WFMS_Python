6ff8f86fd0f3cd2a042d2eb87cf37d99f62d44e1
Evgeny <evgeny@Mac-mini-Evgeny.local>
2023-09-10 18:37:14 +0900

    10.09.2023 18:36 step 15

---
+++ 14.py
@@ -0,0 +1,22 @@
+# year = 1900
+# while year <= 2023:
+#     print(year)
+#     year += 1
+# else:
+#     print('down')
+
+# списки
+# l = [1, 2, 3, 'hello', ['test', 10], 'world']
+# l2 = list('hello')
+# l3 = list((1, 2, 3))
+# l4 = [i for i in 'hello']
+# l5 = [i for i in 'hello world' if i not in['a', 'e', 'i', 'o', 'u', ' ']]
+# print(l, l2, l3, l4, l5, sep='\n')
+
+# print(list(range(0, 11)))
+
+# for i in range(1, 3):
+#     print(f'внешний цикл #{i}')
+#     for j in range(1,3):
+#         print(f'внутренний цикл #{j}')
+
---
+++ 15.py
@@ -0,0 +1,5 @@
+
+for c in range(2, 6):
+    for r in range(2, 11):
+        print(f'{c} * {r} = {c * r}')
+    print(' ')
